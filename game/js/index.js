e8d2779763ed3d35f25226b74fa86c4cf9cf79b9
Amonyous <19dus2@wis.edu.hk>
2015-11-13 09:31:01 +0800

    game

---
+++ game/js/index.js
@@ -0,0 +1,62 @@
+function mygame() {
+
+var username = prompt("What is your name?");
+confirm("Greetings "+ username + ". Are you ready to begin?");
+  
+alert("The knight's of the round table said if you were to kill a cerberus from the depths of hell you will be the new leader of a huge country.");
+var answer = prompt("You consider the challenge. Will you or will you not take the challenge.Yes or No?"); 
+if (answer === "yes")  {
+  alert("You journey off to kill the monster");
+}
+else {
+  
+alert("If you chose no, then you lived in misery never  avenging your mother's death");
+}
+
+alert("While walking along the road you come across a old lady that looks like your mother lying dead you went to reach for her but she just dissapeared this is called the forest of misery.");
+var answer = prompt("You tried to chase after your dead mother but failed because each time you tried to reach for her she just slips away from your grasp. Will you give up to fate or carry on?");
+if(answer === "carry on")
+  alert("You finally had the will to focus on one task and forgotten about your dead mother and bravely marched across the dark, scary, creepy forest.");
+  else{
+
+   alert("if you chose to give up to fate, then you lay down and start crying and soon the ghouls come and take your soul away and torture it.");
+  }
+alert("You travel through a dark cave, deep within it was a sleeping dragon which was never woken up since the dragon age but now you are in the golden age. You travel through the cave, batting off man-eating bats and finally came across the sleeping dragon.");
+
+var answer = prompt("Will you retreat or fight?")
+if(answer === "fight") {
+  alert("You tried to attack it, managed to take off some of it's scales but otherwise it was unharmed you took out excalibur and charged like a bull and cutted its head off proud of what you did you took the dragon's gold, there were also rubies as well.");
+}
+  else {
+  alert("If you chose to retreat, You melt to death by the dragon's fireball.")
+} 
+
+alert("You face a hard choice your backpack could only take one of the two")
+var answer = prompt("Which will you choose? rubies or gold?")
+if(answer === "gold") 
+  alert("you take the gold and journey off to a city called Camelot, this city if famous for they're fashion walks. You book in at a hotel and go to sleep.")
+  else
+    alert("if you chose rubies, you will be cursed because the owner of the rubies was actually a evil salamancer and cursed the rubies, you will be tortured and will eventually rot in tartarus.")
+   
+    alert("The next day a messenger comes and asks for you to join a fashion walk because one of the fashion walk team was injured so the messenger was sent to ask for your help.")
+    
+var answer = prompt("Will you say yes or refuse this offer?")
+
+if(answer === "refuse")
+  alert("you journey off towards the den of the monster. You arrive at the cave entrance by sun down.")
+  else
+    alert(" if you said yes, you get murdered because the whole audience was full of terrorists they charge at you and use a knife that slits your whole fashion team members throat then burns the body.")
+   alert("when you entered the dark creepy cave you saw 3 cute dogs chasing each other.")
+var answer = prompt("Will you kill them or go over and give them a home?")
+if(answer === "kill them")
+  alert("You march over and thrust your moonlit excalibur which then turns into three swords and stabs their heads the dogs die growling")
+  else
+    alert("if you chose go over and give them a home, they suddenly turn into three giant heads and their body form the shape of a bull not a dog and you get eaten and the remains of your body will be sent to the scorpions to test their poison on your body")
+alert("CONGRATULATIONS, you finished my small pop-up game! Please rate it 1-10, 1 is the worst 10 is the best.") 
+  var answer = prompt("Please choose a number 1 to 10!")
+  
+  alert("THANK U...!")
+    
+    
+    
+    }
